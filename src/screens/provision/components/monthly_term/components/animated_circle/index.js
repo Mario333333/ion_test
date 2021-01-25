ce92ff14a32d6c239e7eb1a795e5f941f9c4b1e9
Mario Cesar Gaytan Cruz <mariocgc@icloud.com>
2021-01-25 14:09:02 -0600

    new-animation

--- src/screens/provision/components/monthly_term/components/animated_circle/index.js
+++ src/screens/provision/components/monthly_term/components/animated_circle/index.js
@@ -3,6 +3,9 @@ import React, { useEffect, useState } from 'react';
 
 import {
   View,
+  StyleSheet,
+  Animated,
+  Easing
 } from 'react-native';
 
 import ProgressCircle from 'react-native-progress-circle'
@@ -10,30 +13,64 @@ import ProgressCircle from 'react-native-progress-circle'
 
 
 const AnimatedCircle = (props) => {
-  const [percent, setPercent] = useState(0)
+
+  let rotateValue = new Animated.Value(0)
 
   useEffect(() => {
-    setInterval(() => {
-      setPercent(Math.floor(Math.random() * (100 - 1 + 1) + 1))
-    }, 3000);
+    rotate()
   }, [])
 
-  return (
-    <View >
-      <ProgressCircle
-        percent={percent}
-        radius={191 / 2}
-        borderWidth={24}
-        color="#bb64c8"
-        shadowColor="#e4f4fd"
-        bgColor="#fff"
-      >
-      </ProgressCircle>
+  const rotate = () => {
+    rotateValue.setValue(0)
+    Animated.timing(
+      rotateValue,
+      {
+        toValue: 1,
+        duration: 1000,
+        useNativeDriver: true,
+        easing: Easing.linear,
+      }
+    ).start(() => rotate())
+  }
 
-    </View>
+  const valuesToRotate = rotateValue.interpolate({
+    inputRange: [0, 1],
+    outputRange: ["0deg", "360deg"]
+  })
+
+  return (<View >
+    <Animated.View
+      style={styles.circleShadow} />
+
+    <Animated.View style={{
+      ...styles.roatingCircle,
+      transform: [{ rotate: valuesToRotate }]
+    }} />
+
+  </View>
   );
 };
 
 
+const styles = StyleSheet.create({
+
+  circleShadow: {
+    position: "absolute",
+    width: 190,
+    height: 190,
+    borderRadius: 95,
+    backgroundColor: "rgba(32, 161, 202, 0.1)",
+  },
+  roatingCircle: {
+    width: 190,
+    height: 190,
+    backgroundColor: "white",
+    borderRadius: 95,
+    borderWidth: 24,
+    borderColor: "#bb64c8",
+    borderEndColor: "transparent"
+  },
+
+});
 
 export default AnimatedCircle;
